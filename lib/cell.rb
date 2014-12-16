7592d669241e666c174acbe4fc39568c26be2b33
India Dearlove <india_dearlove@yahoo.co.uk>
2014-12-16 12:40:51 +0000

    Shooting status etcc

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,7 +1,32 @@
 class Cell
 
-  def empty
-    true
+  def initialize
+    @cell = :empty
+    @shot = :not_shot
+  end
+
+  def cell_status
+    @cell
+  end
+
+  def shoot_status
+    @shot
+  end
+
+  def shot
+    @shot = :shot
+  end
+
+  def place_ship
+    @cell = :not_empty
+  end
+
+  def hit_status
+    if @cell == :not_empty
+      return :hit
+    else
+      return :miss
+    end
   end
 
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -3,9 +3,30 @@ require 'cell'
 describe "Cell" do 
 
   let (:cell) {Cell.new}
+  let (:ship) {double :ship}
 
-  it "should be empty" do
-    expect(cell.empty).to eq(true)
+  it "status should be empty" do
+    expect(cell.cell_status).to eq(:empty)
+  end
+
+  it "should know when it is occupied" do
+    cell.place_ship
+    expect(cell.cell_status).to eq(:not_empty)
+  end
+
+  it "should be able to get shot" do
+    expect(cell.shoot_status).to eq(:not_shot)
+  end
+
+  it "should be able to report shoot status when shot" do
+    cell.shot
+    expect(cell.shoot_status).to eq(:shot)
+  end
+
+  it "should be able to tell you if it has been hit" do
+    cell.place_ship
+    cell.shot
+    expect(cell.hit_status).to eq(:hit)
   end
 
 end
