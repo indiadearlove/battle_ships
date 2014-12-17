c9c89cb20069430c7ac58859e21c2e628cb93945
India Dearlove <india_dearlove@yahoo.co.uk>
2014-12-17 16:55:02 +0000

    board edits

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,5 @@
 require_relative = 'cell.rb'
+require_relative = 'ship.rb'
 
 class Board
 
@@ -7,7 +8,20 @@ class Board
   end
 
   def cells_in_grid
-    Array.new(10){ Array.new(10) {Cell.new}}
+    @board = Array.new(10){ Array.new(10) {Cell.new}}
   end
+
+  #def coordinating_cells(coordinate)
+   # first = coordinate[0].ord-97
+    #second = coordinate[1].to_i -1
+  #end
+  
+  #def coordinating_cells
+   # (0...10).each do |row|
+    #  (0...10).each do |column|
+
+
+  #def place_ship(ship, coordinate)
+   # cell
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,7 +3,8 @@ require 'board'
 describe 'Board' do
 
   let (:board) {Board.new}
-  let (:cell) {double :cell}
+  let (:ship) {double :ship}
+  let (:destroyer) {double :destroyer, :size => 2}
 
   it 'should exist' do
     expect(board.exist).to eq(true)
@@ -12,5 +13,24 @@ describe 'Board' do
   it 'should have a grid of 100 cells in a grid' do
     expect(board.cells_in_grid.flatten.count).to eq(100)
   end
+
+  it 'should know how to place a ship' do
+    expect(board.cells_in_grid[0][0].place_ship(:ship)).to eq(:not_empty)
+  end
+
+  it 'should know if a cell is empty' do
+    expect(board.cells_in_grid[0][1].cell_status).to eq(:empty)
+  end
+
+  xit 'should know that each cell has a coordinate' do
+    expect(board.coordinating_cells('a1')).to eq([0][0])
+  end
+
+
+  xit 'should know how to place a ship position [0][0],[0][1]' do
+    board.place_ship(destroyer, [0][0] )
+    expect(board.cells_in_grid)
+  end
+    
 
 end
